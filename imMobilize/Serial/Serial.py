2f51746989340d953e4a567765435b756b702db4
SillyBarrel <endre54@hotmail.com>
2021-03-01 13:13:03 +0100

    Added serial.py with getPort(); function and basic code to the arduino

--- imMobilize/Serial/Serial.py
+++ imMobilize/Serial/Serial.py
@@ -6,14 +6,20 @@ from time import sleep
 arduino_ports = [
     p.device
     for p in serial.tools.list_ports.comports()
-    if 'CH3' in p.description
+# "Dette er den spesifikke porten til min (surface pro 7 - Endre)"
+# Kjør koden under for å finne riktig port: 
+# ports = list(serial.tools.list_ports.comports())
+# for p in ports:
+#   print(p)
+    if 'USB2.0-Serial' in p.description 
 ]
 if not arduino_ports:
     raise IOError("Culd not find the arduino")
 if len(arduino_ports) > 1:
     warnings.warn('Multiple Arduinos found - using the first')
 
-ser = serial.Serial(arduino_ports[0])
+ser = serial.Serial(arduino_ports[0],115200)
+#Liten funksjonstest
 sleep(1)
 ser.write(b'ir255')
 sleep(4)
